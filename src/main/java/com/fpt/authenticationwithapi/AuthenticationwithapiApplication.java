72aae86806799983bbf202af5d765b5042763279
TangYuCheng <42094883+thongchuthanh2000@users.noreply.github.com>
2021-06-18 11:02:54 +0700

    Create Spring boot

---
+++ src/main/java/com/fpt/authenticationwithapi/AuthenticationwithapiApplication.java
@@ -0,0 +1,13 @@
+package com.fpt.authenticationwithapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenticationwithapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthenticationwithapiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fpt/authenticationwithapi/AuthenticationwithapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fpt.authenticationwithapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationwithapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
